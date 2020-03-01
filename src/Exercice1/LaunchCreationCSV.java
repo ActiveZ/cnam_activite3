84457ffaaa45dbac45e47f89df8a486223e382bc
ActiveZ <activez.git@gmail.com>
2020-03-02 00:29:26 +0100

    generateur csv ok

--- src/Exercice1/LaunchCreationCSV.java
+++ src/Exercice1/LaunchCreationCSV.java
@@ -1,4 +1,11 @@
 package Exercice1;
+/*Arnaud Couturier
+ * mars 2020
+ * Générateur de fichier CSV
+ * crée les fichiers habitationsIndiv.csv et habitationsPro.csv dans mon dossier perso bdd-csv
+ * paramétrage possible du nombre de ligne à écrire et du séparateur utilisé (; par défaut)
+ * */
+
 
 import java.io.File;
 import java.io.FileWriter;
@@ -6,7 +13,16 @@ import java.io.IOException;
 import java.util.Random;
 import java.util.Scanner;
 
+
 public class LaunchCreationCSV {
+    final static String[] TAB_CP = {"75000", "13000", "69000", "31000", "06000",
+            "44000", "34000", "67000", "33000", "59000"};
+    final static String[] TAB_VILLES = {"Paris", "Marseille","Lyon","Toulouse","Nice",
+            "Nantes", "Montpellier", "Strasbourg", "Bordeaux", "Lille"};
+    static int nbLigne = 0;  // nombre de ligne à écrire dans le fichier CSV
+    static String separateur = ";"; // séparateur de colonnes par défaut
+    static String cheminPerso = System.getProperty("user.dir") + "/bdd-csv/"; // mon dossier local
+
 
     public static void main(String[] args) {
         Scanner sc = new Scanner((System.in));
@@ -22,9 +38,11 @@ public class LaunchCreationCSV {
             } while (choix < 1 | choix > 3);
             switch (choix) {
                 case 1:
+                    saisirParametres();
                     createHabIndvCSV();
                     break;
                 case 2:
+                    saisirParametres();
                     createHabProCSV();
                     break;
                 case 3:
@@ -36,18 +54,10 @@ public class LaunchCreationCSV {
 
     }
 
-    private static void createHabIndvCSV () {
-        final String[] TAB_CP = {"75000", "13000", "69000", "31000", "06000",
-                "44000", "34000", "67000", "33000", "59000"};
-        final String[] TAB_VILLES = {"Paris", "Marseille","Lyon","Toulouse","Nice",
-                "Nantes", "Montpellier", "Strasbourg", "Bordeaux", "Lille"};
-
-        String fichierCSV  = "habitationsIndiv.csv";
-        String separateur = ";";
+    // Saisie des paramètres utilisateurs: nombre de ligne du fichier CSV et séparateur de colonne à utiliser
+    private  static void saisirParametres() {
         Scanner sc = new Scanner(System.in);
-        Random rd = new Random();
 
-        int nbLigne = 0;
         do {
             System.out.println("Nombre de ligne ? ");
             nbLigne = sc.nextInt();
@@ -57,9 +67,16 @@ public class LaunchCreationCSV {
         System.out.println("Séparateur ? (défaut = " + separateur + ")");
         String choix = sc.nextLine();
         if (!choix.isEmpty()) {separateur = choix;}
+    }
 
-        fichierCSV =  System.getProperty("user.dir") + "/bdd-csv/" + fichierCSV ;
-        File f = new File(fichierCSV  );
+    // création du fichier habitationsIndiv.csv
+    private static void createHabIndvCSV () {
+        String fichierCSV  = "habitationsIndiv.csv";
+        Random rd = new Random();
+
+        // installation dans mon dossier perso
+        fichierCSV =  cheminPerso + fichierCSV ;
+        File f = new File(fichierCSV);
 
         try {
             // création du fichierCSV , écrasement de l'ancien éventuel
@@ -84,7 +101,34 @@ public class LaunchCreationCSV {
         }
     }
 
+    // Création du fichier habitationsPro.csv
     private static void createHabProCSV () {
+        String fichierCSV  = "habitationsPro.csv";
+        Random rd = new Random();
 
+        // installation dans mon dossier perso
+        fichierCSV =  cheminPerso + fichierCSV ;
+        File f = new File(fichierCSV);
+
+        try {
+            // création du fichierCSV , écrasement de l'ancien éventuel
+            FileWriter fw = new FileWriter(f, false);
+            // noms des colonnes
+            String txt = "nom" + separateur + "adresse" + separateur + "surface" + separateur + "nbEmployes\n";
+            fw.write(txt);
+            for (int i = 0; i < nbLigne; i++) {
+                txt = "nom" + i + separateur; // nom du propriétaire
+                int rnd = rd.nextInt (TAB_CP.length); // CP + ville
+                txt += TAB_CP[rnd] + " " + TAB_VILLES[rnd] + separateur +
+                        (rd.nextInt(951) + 50) + separateur + // surface entre 50 et 1000 m²
+                        rd.nextInt(101) + "\n"; // entre 0 et 100 employés
+                // écriture de la ligne
+                fw.write(txt);
+            }
+            fw.close();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
