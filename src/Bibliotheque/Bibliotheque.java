f8584ba11dd777a49802d5160f3ac66fb32e1df0
ActiveZ <activez.git@gmail.com>
2020-03-05 00:59:16 +0100

    case 1 en cours

--- src/Bibliotheque/Bibliotheque.java
+++ src/Bibliotheque/Bibliotheque.java
@@ -5,25 +5,33 @@ import java.util.List;
 
 public class Bibliotheque {
     private List<Document> bibliotheque = new ArrayList<Document>();
-    private final int MAX_DOC = 1000; // valeur max par défaut si instanciation ou modification hors limites [1..1000]
-    private int maxDoc = 0; //nb max de documents
+    private final int MAX_DOC = 1000; // valeur max par défaut si instanciation ou modification hors limites [10..1000]
+    // TODO: 05/03/2020 compteur statique pr nb instanciation  et eviter ecrasement involantaire de la biblio en cours
 
+    private final int MIN_DOC = 10; // valeur min par défaut
+
+    private int maxDoc = 1000; //nb max de documents
     ////////////////// contructeur ////////////////////////
+
     public Bibliotheque(int maxDoc) {
         setMaxDoc(maxDoc);
         this.maxDoc = getMaxDoc();
     }
-
     /////////////////// ACCESSEURS /////////////////////////
+
+    public int getMIN_DOC() {
+        return MIN_DOC;
+    }
+
     public int getMaxDoc() {
         return maxDoc;
     }
 
-    public void setMaxDoc(int maxDoc) {
-        if (maxDoc < 1 | maxDoc > MAX_DOC) {
-            System.out.println("ERREUR: la nombre maximum de documents doit être compris entre 1 et 1000");
+    public void setMaxDoc(int maxDoc) { // errreur cas ou < au nb deja presents
+        if (maxDoc < MIN_DOC | maxDoc > MAX_DOC) {
+            System.out.println("ERREUR: la nombre maximum de documents doit être compris entre " + MIN_DOC + " et " + MAX_DOC);
             if (maxDoc == 0) { // cas de l'intanciation invalide
-                System.out.println("Application de la valeur par défaut: " + MAX_DOC);
+                System.out.println("Application de la valeur max par défaut: " + MAX_DOC);
                 this.maxDoc = MAX_DOC;
             } else { // cas de modification invalide
                 System.out.println("Valeur en cours inchangée: " + maxDoc);
@@ -39,8 +47,8 @@ public class Bibliotheque {
 
     // renvoi le ième document
     public void getDocument(int i) {
-        if (i > nbOuvrages()) {
-            System.out.println("ERREUR: ce document n'existe pas dans la bibliothèque qui contient " + nbOuvrages() + " documents");
+        if (i > getNbOuvrages()) {
+            System.out.println("ERREUR: ce document n'existe pas dans la bibliothèque qui contient " + getNbOuvrages() + " documents");
         } else {
             System.out.println(bibliotheque.get(i).toString());
         }
@@ -63,8 +71,8 @@ public class Bibliotheque {
 
     }
 
-    public boolean bibliothequeExiste() { return maxDoc != 0;} // évite une double instanciation
+    //public boolean existe() { return maxDoc != 0;} // évite une double instanciation...inutile + erreur => mettre compteur statique
 
-    private int nbOuvrages() {return bibliotheque.size();}
+    public int getNbOuvrages() {return bibliotheque.size();}
 
 }
--- src/Bibliotheque/Menu.java
+++ src/Bibliotheque/Menu.java
@@ -4,10 +4,11 @@ import java.util.Scanner;
 
 public class Menu {
     private Scanner sc = new Scanner(System.in);
+    private static Bibliotheque bibliotheque;
 
     public void menuGeneral() {
         do {
-            System.out.println("1: Créer bibliothèque (et 10 documents");
+            System.out.println("\n1: Créer bibliothèque (et 10 documents)");
             System.out.println("2: Modifier taille bibliothèque");
             System.out.println("3: Afficher tous les ouvrages");
             System.out.println("4: Afficher le nième document");
@@ -19,8 +20,14 @@ public class Menu {
             String choix = "";
             System.out.print("Votre choix: ");
             choix = sc.nextLine();
+            System.out.println("------------------------------------");
             switch (choix) {
-                case "1":
+                case "1": // Créer bibliothèque (et 10 documents)
+                    System.out.println("Taille de la bibliothèque entre 10 et 1000 documents):");
+                    int taille = sc.nextInt();
+                    bibliotheque = new Bibliotheque(taille);
+                    peupleBiblio(); // créer 10 documents pour la démo
+                    System.out.println("Bibliothèque créee ! Taille max:" + bibliotheque.getMaxDoc() + ". Nombre de documents:" + bibliotheque.getNbOuvrages());
                     break;
                 case "2":
                     break;
@@ -43,4 +50,8 @@ public class Menu {
         } while (true);
     }
 
+    // remplir la bibliothèque avec 10 documents pour la démo
+    private void peupleBiblio() {
+
+    }
 }
